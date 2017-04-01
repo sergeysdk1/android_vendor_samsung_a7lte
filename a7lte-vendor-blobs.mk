601d40d2fcb2ee925cc28149c8711b8ee70478da
sergeysdk1 <sdk@fromru.com>
2017-04-01 16:21:09 +0300

    Use camera wraper
    Change-Id: I4f64dfe02e5088074f37e6162e54e3ab32533fca

--- a7lte-vendor-blobs.mk
+++ a7lte-vendor-blobs.mk
@@ -74,7 +74,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
-    $(LOCAL_PATH)/proprietary/lib/hw/camera.msm8916.so:system/lib/hw/camera.msm8916.so \
+    $(LOCAL_PATH)/proprietary/lib/hw/camera.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
     $(LOCAL_PATH)/proprietary/lib/libarcsoft_night_shot_ex.so:system/lib/libarcsoft_night_shot_ex.so \
     $(LOCAL_PATH)/proprietary/lib/libarcsoft_panorama_burstcapture.so:system/lib/libarcsoft_panorama_burstcapture.so \
     $(LOCAL_PATH)/proprietary/lib/libmmcamera_cac2_lib_13M.so:system/lib/libmmcamera_cac2_lib_13M.so \
